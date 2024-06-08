dfdcd1a2626cdf6833565a3630ebb07c7c5c7a44
Parag Khare <poonamkhare@Parags-MacBook-Pro.local>
2024-06-08 23:48:46 +0100

    test for genkins

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -3,7 +3,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/features",glue="StepDefinition")
+@CucumberOptions(features="src/test/features",glue="StepDefinition",plugin = { "pretty", "html:target/cucumber-reports.html" })
 public class Runner {
 
 
